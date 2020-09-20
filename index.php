220d66bbce5e1d72af1b8c4cec3c8057eeaf3436
angiepalacios16 <palaciospedrazaa@gmail.com>
2020-09-19 21:49:48 -0500

    [ADD] Add gallery view and layout

--- index.php
+++ index.php
@@ -32,7 +32,7 @@
         <h3 class="masthead-brand text-primary">Test Good</h3>
         <nav class="nav nav-masthead justify-content-center">
           <a class="nav-link active" href="#">Inicio</a>
-          <a class="nav-link" href="#">Galeria</a>
+          <a class="nav-link" href="app/views/gallery.php">Galeria</a>
           <a class="nav-link" href="#">Video y audio</a>
           <a class="nav-link" href="#">Contacto</a>
         </nav>
@@ -54,7 +54,7 @@
     </footer>
   </div>
 </body>
-<script src="app/assets/jquery/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-<script src="app/assets/jquery/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<!-- <script src="app/assets/jquery/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="app/assets/jquery/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script> -->
 
 </html>
